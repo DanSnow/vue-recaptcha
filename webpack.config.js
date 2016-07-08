505d59ae17eb8dae80e5ff5f6e8d87e224a94aac
DanSnow <dododavid006@gmail.com>
2016-07-08 23:20:25 +0800

    Lint

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,23 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: './dist',
-    filename: "vue-recaptcha.js",
-    library: "VueRecaptcha",
-    libraryTarget: "umd"
+    filename: 'vue-recaptcha.js',
+    library: 'VueRecaptcha',
+    libraryTarget: 'umd'
   },
   externams: {
     vue: 'Vue'
   },
   module: {
     loaders: [{
-        test: /\.vue$/,
-        loader: 'vue'
-      },{
-        test: /\.js$/,
-        loader: 'babel!eslint',
+      test: /\.vue$/,
+      loader: 'vue'
+    }, {
+      test: /\.js$/,
+      loader: 'babel!eslint',
         // make sure to exclude 3rd party code in node_modules
-        exclude: /node_modules/
-      }]
+      exclude: /node_modules/
+    }]
   },
   // vue-loader config:
   // lint all JavaScript inside *.vue files with ESLint
