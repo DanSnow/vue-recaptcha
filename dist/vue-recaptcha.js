4543595e7694bb1e029c05abd38fdf1e195aea67
circleci <DanSnow@users.noreply.github.com>
2016-11-29 01:59:29 +0000

    ci: Compile [skip ci]

--- dist/vue-recaptcha.js
+++ dist/vue-recaptcha.js
@@ -1,246 +1,121 @@
-(function webpackUniversalModuleDefinition(root, factory) {
-	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory();
-	else if(typeof define === 'function' && define.amd)
-		define([], factory);
-	else if(typeof exports === 'object')
-		exports["VueRecaptcha"] = factory();
-	else
-		root["VueRecaptcha"] = factory();
-})(this, function() {
-return /******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global.VueRecaptcha = factory());
+}(this, (function () { 'use strict';
 
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
+var defer = function defer() {
+  var deferred = {};
+  deferred.promise = new Promise(function (resolve) {
+    deferred.resolve = resolve;
+  });
+  return deferred;
+};
 
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
-/******/ 			return installedModules[moduleId].exports;
+function createRecaptcha() {
+  var recaptcha = null;
+  var deferred = defer();
 
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
-/******/ 		};
+  return {
+    setRecaptcha: function setRecaptcha(recap) {
+      recaptcha = recap;
+      deferred.resolve(recap);
+    },
+    getRecaptcha: function getRecaptcha() {
+      if (recaptcha) {
+        return Promise.resolve(recaptcha);
+      }
 
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+      return deferred.promise;
+    },
+    render: function render(ele, key, options) {
+      return this.getRecaptcha().then(function (recap) {
+        var opts = Object.assign({}, { sitekey: key }, options);
+        return recap.render(ele, opts);
+      });
+    },
+    reset: function reset(widgetId) {
+      if (typeof widgetId === 'undefined') {
+        return;
+      }
 
-/******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
+      this.assertRecaptchaLoad();
+      recaptcha.reset(widgetId);
+    },
+    checkRecaptchaLoad: function checkRecaptchaLoad() {
+      if (window.hasOwnProperty('grecaptcha')) {
+        this.setRecaptcha(window.grecaptcha);
+      }
+    },
+    assertRecaptchaLoad: function assertRecaptchaLoad() {
+      if (recaptcha === null) {
+        throw new Error('ReCAPTCHA has not been loaded');
+      }
+    }
+  };
+}
 
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
+var recaptcha = createRecaptcha();
 
+window.vueRecaptchaApiLoaded = function () {
+  recaptcha.setRecaptcha(window.grecaptcha);
+};
 
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
+var widgetId = null;
 
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
+var VueRecaptcha$1 = {
+  props: {
+    key: {
+      type: String,
+      required: true
+    },
+    options: {
+      type: Object,
+      default: function _default() {
+        return {};
+      }
+    }
+  },
+  created: function created() {
+    recaptcha.checkRecaptchaLoad();
+  },
+  mounted: function mounted() {
+    var self = this;
+    var opts = Object.assign({}, this.options, {
+      callback: this.emitVerify,
+      'expired-callback': this.emitExpired
+    });
+    recaptcha.render(this.$refs.container, this.key, opts).then(function (id) {
+      widgetId = id;
+      self.$emit('render', widgetId);
+    });
+  },
 
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
+  methods: {
+    reset: function reset() {
+      recaptcha.reset(widgetId);
+    },
+    emitVerify: function emitVerify(response) {
+      this.$emit('verify', response);
+    },
+    emitExpired: function emitExpired() {
+      this.$emit('expired');
+    }
+  },
+  events: {
+    recaptchaReset: function recaptchaReset() {
+      this.reset();
+    }
+  },
+  render: function render(h) {
+    return h(
+      'div',
+      { ref: 'container' },
+      []
+    );
+  }
+};
 
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
-/******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ function(module, exports, __webpack_require__) {
+return VueRecaptcha$1;
 
-	'use strict';
-
-	var _recaptcha = __webpack_require__(1);
-
-	var _recaptcha2 = _interopRequireDefault(_recaptcha);
-
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-	module.exports = _recaptcha2.default;
-
-/***/ },
-/* 1 */
-/***/ function(module, exports, __webpack_require__) {
-
-	var __vue_exports__, __vue_options__
-	var __vue_styles__ = {}
-
-	/* script */
-	__vue_exports__ = __webpack_require__(2)
-
-	/* template */
-	var __vue_template__ = __webpack_require__(4)
-	__vue_options__ = __vue_exports__ = __vue_exports__ || {}
-	if (
-	  typeof __vue_exports__.default === "object" ||
-	  typeof __vue_exports__.default === "function"
-	) {
-	__vue_options__ = __vue_exports__ = __vue_exports__.default
-	}
-	if (typeof __vue_options__ === "function") {
-	  __vue_options__ = __vue_options__.options
-	}
-
-	__vue_options__.render = __vue_template__.render
-	__vue_options__.staticRenderFns = __vue_template__.staticRenderFns
-
-	module.exports = __vue_exports__
-
-
-/***/ },
-/* 2 */
-/***/ function(module, exports, __webpack_require__) {
-
-	'use strict';
-
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-
-	var _recaptcha = __webpack_require__(3);
-
-	var _recaptcha2 = _interopRequireDefault(_recaptcha);
-
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-	var widgetId = null; //
-	//
-	//
-	//
-
-	exports.default = {
-	  props: {
-	    key: {
-	      type: String,
-	      required: true
-	    },
-	    options: {
-	      type: Object,
-	      default: function _default() {
-	        return {};
-	      }
-	    }
-	  },
-	  created: function created() {
-	    _recaptcha2.default.checkRecaptchaLoad();
-	  },
-	  mounted: function mounted() {
-	    var self = this;
-	    var opts = Object.assign({}, this.options, {
-	      callback: this.emitVerify,
-	      'expired-callback': this.emitExpired
-	    });
-	    _recaptcha2.default.render(this.$refs.container, this.key, opts).then(function (id) {
-	      widgetId = id;
-	      self.$emit('render', widgetId);
-	    });
-	  },
-
-	  methods: {
-	    reset: function reset() {
-	      _recaptcha2.default.reset(widgetId);
-	    },
-	    emitVerify: function emitVerify(response) {
-	      this.$emit('verify', response);
-	    },
-	    emitExpired: function emitExpired() {
-	      this.$emit('expired');
-	    }
-	  },
-	  events: {
-	    recaptchaReset: function recaptchaReset() {
-	      this.reset();
-	    }
-	  }
-	};
-	module.exports = exports['default'];
-
-/***/ },
-/* 3 */
-/***/ function(module, exports) {
-
-	'use strict';
-
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	exports.createRecaptcha = createRecaptcha;
-	var defer = function defer() {
-	  var deferred = {};
-	  deferred.promise = new Promise(function (resolve) {
-	    deferred.resolve = resolve;
-	  });
-	  return deferred;
-	};
-
-	function createRecaptcha() {
-	  var recaptcha = null;
-	  var deferred = defer();
-
-	  return {
-	    setRecaptcha: function setRecaptcha(recap) {
-	      recaptcha = recap;
-	      deferred.resolve(recap);
-	    },
-	    getRecaptcha: function getRecaptcha() {
-	      if (recaptcha) {
-	        return Promise.resolve(recaptcha);
-	      }
-
-	      return deferred.promise;
-	    },
-	    render: function render(ele, key, options) {
-	      return this.getRecaptcha().then(function (recap) {
-	        var opts = Object.assign({}, { sitekey: key }, options);
-	        return recap.render(ele, opts);
-	      });
-	    },
-	    reset: function reset(widgetId) {
-	      if (typeof widgetId === 'undefined') {
-	        return;
-	      }
-
-	      this.assertRecaptchaLoad();
-	      recaptcha.reset(widgetId);
-	    },
-	    checkRecaptchaLoad: function checkRecaptchaLoad() {
-	      if (window.hasOwnProperty('grecaptcha')) {
-	        this.setRecaptcha(window.grecaptcha);
-	      }
-	    },
-	    assertRecaptchaLoad: function assertRecaptchaLoad() {
-	      if (recaptcha === null) {
-	        throw new Error('ReCAPTCHA has not been loaded');
-	      }
-	    }
-	  };
-	}
-
-	var recaptcha = createRecaptcha();
-
-	window.vueRecaptchaApiLoaded = function () {
-	  recaptcha.setRecaptcha(window.grecaptcha);
-	};
-
-	exports.default = recaptcha;
-
-/***/ },
-/* 4 */
-/***/ function(module, exports) {
-
-	module.exports={render:function (){var _vm=this;
-	  return _vm._h('div', {
-	    ref: "container"
-	  })
-	},staticRenderFns: []}
-
-/***/ }
-/******/ ])
-});
-;
+})));
