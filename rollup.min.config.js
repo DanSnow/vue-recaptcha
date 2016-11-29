cd503b5f9e51429d881e18888d1aabb46950b25b
DanSnow <dododavid006@gmail.com>
2016-11-29 13:02:32 +0800

    chore(rollup): Add config for minify version

---
+++ rollup.min.config.js
@@ -0,0 +1,7 @@
+import config from './rollup.config'
+import uglify from 'rollup-plugin-uglify';
+
+config.dest = 'dist/vue-recaptcha.min.js';
+config.plugins.push(uglify());
+
+export default config;
