c6c9b93cc9c745a00bd7d278f2c6cf5013ad136a
DanSnow <dododavid006@gmail.com>
2017-01-12 12:16:30 +0800

    chore: Lint

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import commonjs from 'rollup-plugin-commonjs'
+import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
