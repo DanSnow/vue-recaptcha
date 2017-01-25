121cb9e1804b76bfe3fcd00d3a54c096c48354c8
circleci <DanSnow@users.noreply.github.com>
2017-01-25 16:43:02 +0000

    ci: Compile [skip ci]

--- dist/vue-recaptcha.js
+++ dist/vue-recaptcha.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global.VueRecaptcha = factory());
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define(factory) :
+	(global.VueRecaptcha = factory());
 }(this, (function () { 'use strict';
 
 var _extends = Object.assign || function (target) {
