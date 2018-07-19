bc77c7644b5576e7605efd85deeec1ae2af714a0
DanSnow <dododavid006@gmail.com>
2018-07-19 10:32:57 +0800

    e2e: Test vue-recaptcha.min.js

--- e2e/e2e.spec.js
+++ e2e/e2e.spec.js
@@ -1,4 +1,4 @@
-import HttpServer from 'http-server'
+import { createServer } from 'http-server'
 import path from 'path'
 import puppeteer from 'puppeteer'
 
@@ -6,37 +6,66 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000 // Set timeout
 
 const URL = 'http://localhost:8080/e2e/index.html'
 
+let page
+
+function runE2ETest (script) {
+  describe(script, () => {
+    describe('Normal reCAPTCHA', () => {
+      it('work', async () => {
+        const frames = await page.frames()
+        const recaptchaFrame = frames[1]
+
+        const $recaptcha = await recaptchaFrame.$('.recaptcha-checkbox')
+        await $recaptcha.click()
+        return page.waitFor('#normal-verified')
+      })
+    })
+
+    describe('Binded reCAPTCHA', () => {
+      it('work', async () => {
+        const $btn = await page.$('#binded')
+        await $btn.click()
+        return page.waitFor('#binded-verified')
+      })
+    })
+
+    describe('Invisible reCAPTCHA', () => {
+      it('work', async () => {
+        const $btn = await page.$('#submit')
+        await $btn.click()
+        return page.waitFor('#invisible-verified')
+      })
+    })
+
+    beforeEach(async () => {
+      await page.goto(URL)
+      await page.waitFor('#normal-verified', { polling: 'mutation' })
+      await page.$eval(
+        'body',
+        ($el, script) =>
+          new Promise(resolve => {
+            let $script = document.createElement('script')
+            $script.defer = true
+            $script.src = `../dist/${script}`
+            $script.addEventListener('load', () => {
+              // eslint-disable-next-line no-undef
+              bootstrap()
+              resolve()
+            })
+            $el.appendChild($script)
+          }),
+        script
+      )
+      await page.waitFor(3000)
+    })
+  })
+}
+
 describe('e2e', () => {
   let browser
-  let page
   let server
-
-  describe('Normal reCAPTCHA', () => {
-    it('work', async () => {
-      const frames = await page.frames()
-      const recaptchaFrame = frames[1]
-
-      const $recaptcha = await recaptchaFrame.$('.recaptcha-checkbox')
-      await $recaptcha.click()
-      return page.waitFor('#normal-verified')
-    })
-  })
-
-  describe('Binded reCAPTCHA', () => {
-    it('work', async () => {
-      const $btn = await page.$('#binded')
-      await $btn.click()
-      return page.waitFor('#binded-verified')
-    })
-  })
-
-  describe('Invisible reCAPTCHA', () => {
-    it('work', async () => {
-      const $btn = await page.$('#submit')
-      await $btn.click()
-      return page.waitFor('#invisible-verified')
-    })
-  })
+  runE2ETest('vue-recaptcha.js')
+  runE2ETest('vue-recaptcha.min.js')
 
   beforeAll(() => {
     // Setup http-server
@@ -49,7 +78,7 @@ describe('e2e', () => {
         })
         .then(x => (page = x)),
       new Promise(resolve => {
-        server = HttpServer.createServer({
+        server = createServer({
           root: path.resolve(__dirname, '..')
         })
 
@@ -58,11 +87,6 @@ describe('e2e', () => {
         })
       })
     ])
-  })
-
-  beforeEach(async () => {
-    await page.goto(URL)
-    await page.waitFor(3000)
   })
 
   afterAll(() => {
--- e2e/main.js
+++ e2e/main.js
@@ -1,29 +1,32 @@
 /* global Vue: false, VueRecaptcha: false */
 
-// eslint-disable-next-line no-new
-new Vue({
-  el: '#root',
-  data: {
-    sitekey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
-    normalVerified: false,
-    bindedVerified: false,
-    invisibleVerified: false
-  },
-  components: {
-    'vue-recaptcha': VueRecaptcha
-  },
-  methods: {
-    onSubmit: function () {
-      this.$refs.invisibleRecaptcha.execute()
+// eslint-disable-next-line no-unused-vars
+function bootstrap () {
+  // eslint-disable-next-line no-new
+  new Vue({
+    el: '#root',
+    data: {
+      sitekey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
+      normalVerified: false,
+      bindedVerified: false,
+      invisibleVerified: false
     },
-    onNormalVerify () {
-      this.normalVerified = true
+    components: {
+      'vue-recaptcha': VueRecaptcha
     },
-    onBindedVerify () {
-      this.bindedVerified = true
-    },
-    onInvisibleVerify () {
-      this.invisibleVerified = true
+    methods: {
+      onSubmit: function () {
+        this.$refs.invisibleRecaptcha.execute()
+      },
+      onNormalVerify () {
+        this.normalVerified = true
+      },
+      onBindedVerify () {
+        this.bindedVerified = true
+      },
+      onInvisibleVerify () {
+        this.invisibleVerified = true
+      }
     }
-  }
-})
+  })
+}
