9a4d9935e57ec64fae286622fbb712c4b43be8a9
DanSnow <dododavid006@gmail.com>
2023-03-05 19:21:40 +0800

    docs: update index

---
+++ docs/components/content/IndexDemo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex gap-4 mb-8">
+    <div class="w-2/3">
+      <Card class="h-full">
+        <template #title> Easy to use </template>
+        <template #description> Use <code v-pre>&lt;Checkbox /&gt;</code> to render reCAPTCHA checkbox </template>
+      </Card>
+    </div>
+
+    <div class="w-1/3">
+      <ContentSlot :use="$slots.default" />
+    </div>
+  </div>
+</template>
