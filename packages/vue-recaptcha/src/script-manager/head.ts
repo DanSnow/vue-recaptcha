9b5ed05086c2e77074df2444961044ae91494954
Mateus Fernandes <mf@iungoweb.io>
2024-10-02 16:05:43 -0300

    fix(performance): Google reCaptcha redundant loading

--- packages/vue-recaptcha/src/script-manager/head.ts
+++ packages/vue-recaptcha/src/script-manager/head.ts
@@ -4,10 +4,15 @@ import { defineScriptLoader, toQueryString } from './common'
 export const createHeadRecaptcha = defineScriptLoader((options) => {
   return () => {
     onMounted(() => {
+
+      if (document.getElementById('vue-recaptcha'))
+        return
+
       const script = document.createElement('script')
       script.src = `${options.recaptchaApiURL}?${toQueryString(options.params)}`
       script.async = true
       script.defer = true
+      script.id = 'vue-recaptcha'
       if (options.nonce) script.nonce = options.nonce
 
       document.head.append(script)
